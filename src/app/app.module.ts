6ea6477036e0d9d1a38e399cd4459ece1c7f4708
Tatiana Deux <tatiana.deux@lapiscine.pro>
2022-06-03 22:18:18 +0200

    component accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoadingComponent } from './loading/loading.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoadingComponent
+    LoadingComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
